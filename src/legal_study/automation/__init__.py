2fde273306e423274b911e2c00d0bc7be870ca68
mizuidekazuhiro <mizuide.kazuhiro@gmail.com>
2026-09-23 13:07:29 +0900

    feat: export file watcher primitives

--- src/legal_study/automation/__init__.py
+++ src/legal_study/automation/__init__.py
@@ -1,5 +1,11 @@
 """Automation primitives for local study workflow orchestration."""
 
+from legal_study.automation.file_watcher import (
+    FileUpdateEvent,
+    FileUpdateWatcher,
+    WatchedFileSignature,
+    iter_file_updates,
+)
 from legal_study.automation.sync_stability import (
     FileObservation,
     SyncStabilityResult,
@@ -9,7 +15,11 @@ from legal_study.automation.sync_stability import (
 
 __all__ = [
     "FileObservation",
+    "FileUpdateEvent",
+    "FileUpdateWatcher",
     "SyncStabilityResult",
+    "WatchedFileSignature",
+    "iter_file_updates",
     "mark_question_sync_stable",
     "wait_for_sync_stable",
 ]
