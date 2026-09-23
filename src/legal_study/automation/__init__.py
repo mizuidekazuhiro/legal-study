718562944f5bef7f346269fc26ce35cffde0bc66
mizuidekazuhiro <mizuide.kazuhiro@gmail.com>
2026-09-23 13:19:28 +0900

    feat: export persistent queue and recovery primitives

--- src/legal_study/automation/__init__.py
+++ src/legal_study/automation/__init__.py
@@ -12,7 +12,14 @@ from legal_study.automation.orchestrator import (
     changed_current_pages,
     establish_stable_baseline,
     process_pdf_update,
+    recover_watch_startup,
     watch_pdf_updates,
+)
+from legal_study.automation.queue import (
+    AutomationStateStore,
+    WatchState,
+    WorkItem,
+    WorkStatus,
 )
 from legal_study.automation.sync_stability import (
     FileObservation,
@@ -24,18 +31,23 @@ from legal_study.automation.sync_stability import (
 
 __all__ = [
     "AutomationCycleResult",
+    "AutomationStateStore",
     "FileObservation",
     "FileUpdateEvent",
     "FileUpdateWatcher",
     "QuestionAutomationResult",
     "SyncStabilityResult",
+    "WatchState",
     "WatchedFileSignature",
+    "WorkItem",
+    "WorkStatus",
     "changed_current_pages",
     "establish_stable_baseline",
     "iter_file_updates",
     "mark_question_sync_stable",
     "mark_question_sync_stable_from_verified_hash",
     "process_pdf_update",
+    "recover_watch_startup",
     "wait_for_sync_stable",
     "watch_pdf_updates",
 ]
